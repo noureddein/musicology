ac7fb66ff29e787f734fe7307915c14fe74bbbf5
noureddein <noureddein@gmail.com>
2022-02-05 02:49:35 +0200

    toogle menu

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { NavLink } from 'react-router-dom'
 import './styles/navbar.css'
 const searchIcon = 'assets/search.webp';
@@ -9,25 +9,31 @@ const header = ['home', 'products', 'promo', 'about', 'contact']
 
 function NavBar() {
     const [search, setSearch] = useState(false)
+    const navList = useRef()
+    function handleNavigation() {
+        navList.current.classList.toggle('navIsVisible')
+    }
     return (
         <div className='navBarContainer'>
             <div className='logoImgContainer'>
                 <img className='logoImg' src={logo} alt="Logo" />
             </div>
-            <>
-                {/* <div>
-                    <ul className='navigationTitle'>
-
+            <div className='navListsSearchContainer'>
+                <div className="menuIcon" onClick={handleNavigation}>
+                    <i className="fas fa-bars" ></i>
+                </div>
+                <div className='navigationTitleContainer'  >
+                    <ul className='navigationTitle' ref={navList}>
                         {
                             header.map(item => <li key={item}><NavLink className='navLink' to='/'>{item.toUpperCase()}</NavLink></li>)
                         }
                     </ul>
-                </div> */}
+                </div>
                 <div className='search-sec'>
                     <input type="text" className={search ? 'isSearchClicked' : ''} placeholder='Search...' />
                     <img className='searchIcon' onClick={() => setSearch(!search)} src={searchIcon} alt="Search Icon" />
                 </div>
-            </>
+            </div>
         </div>
     );
 }
